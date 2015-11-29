90788310c78e7fedb252d513594b5b6dd376d469
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2015-11-29 13:46:46 +0100

    Simon's SAT->3SAT algorithm.

--- ex3/SAT2Ising.hs
+++ ex3/SAT2Ising.hs
@@ -1,8 +1,35 @@
-{-# LANGUAGE FlexibleInstances, TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances, TypeFamilies, ParallelListComp, TupleSections #-}
 
 import Prelude hiding (not)
 import Control.Arrow
+import Control.Monad
 import Data.Monoid
+
+-- Example run:
+-- @
+-- GHCi> asSAT . getCNF3 $ satTo3sat =<< ((x₁ ∨ x₂ ∨ not x₃ ∨ x₅ ∨ x₇) ∧ (x₂ ∨ x₄ ∨ x₆ ∨ not x₇))
+-- (Lit 1 :∨ (Lit 2 :∨ Lit 8)) :∧ ((Lit 9 :∨ (Lit 5 :∨ Lit 7)) :∧ ((Lit 2 :∨ (Lit 4 :∨ Lit 8)) :∧ (Lit 8 :∨ (Lit 6 :∨ Not (Lit 7)))))
+-- @
+
+satTo3sat :: (Enum a, Ord a) => CNFClause a -> CNF3 a
+satTo3sat xs
+  | n<=3       =  [CNF3Clause xs]
+  
+  | otherwise  =  [CNF3Clause [x₁, x₂, a₁]]
+                 ∧ [CNF3Clause [not a, x, a']
+                   | x <- xMid
+                   | a <- newvarsMid
+                   | a' <- tail newvarsMid ]
+                 ∧ [CNF3Clause [newvars!!(n-4), xs!!(n-2), xs!!(n-1)]]
+ where n = length xs
+       (x₁:x₂:xMid) = take (n-2) xs
+       newvars = (mempty,) <$> newVariables [xs]
+       (a₁:newvarsMid) = take (n-3) newvars
+
+
+
+
+
 
 class Booly b where
   (∨) :: b -> b -> b
@@ -24,6 +51,12 @@ type CNFClause a = [(Negation, a)]
 
 type CNF a = [CNFClause a]
 
+instance Booly (Any, v) where
+  (Any p₁,l₁) ∧ (Any p₂,_) = (Any $ p₁&&p₂, l₁)
+  (Any p₁,l₁) ∨ (Any p₂,_) = (Any $ p₁||p₂, l₁)
+  not (Any True, l) = (Any False, l)
+  not (Any False, l) = (Any True, l)
+
 instance Booly (CNF a) where
   -- conjunction is just concatenation of clause-lists:
   p₁ ∧ p₂ = p₁ ++ p₂
@@ -33,7 +66,7 @@ instance Booly (CNF a) where
   
   -- use De Morgan's law to implement negation of the conjunction of clauses
   -- as disjunction of the individual negations.
-  not (p₁:ps) = [[first (Any False<>) x] | x <- p₁] ∨ not ps
+  not (p₁:ps) = [[first (Any True<>) x] | x <- p₁] ∨ not ps
   not [] = [[]]
 
 instance (Enum a, Ord a) => HasVariables (CNF a) where
@@ -51,45 +84,27 @@ getCNF3 :: CNF3 a -> CNF a
 getCNF3 = map get3Clause
 
 instance (Enum a, Ord a) => HasVariables (CNF3 a) where
-  type VarIndex (CNF a) = a
+  type VarIndex (CNF3 a) = a
   lit x = [CNF3Clause [(Any False, x)]]
   newVariables = newVariables . getCNF3
 
-instance Booly (CNF3 a) where
+instance (Enum a, Ord a) => Booly (CNF3 a) where
   p₁ ∧ p₂ = p₁ ++ p₂
   
-  -- short single-clause formulas can just be merged.
-  [CNF3Clause [x]] ∨ p | all ((<3) . length . get3Clause) p
-                       = map (\(CNF3Clause c) -> CNF3Clause (x:c))
-  p ∨ [CNF3Clause [x]] | all ((<3) . length . get3Clause) p
-                       = map (\(CNF3Clause c) -> CNF3Clause (x:c))
-  -- For larger disjunctions, introduce new variables that represent
-  -- satisfaction of either of the subequations.
-  p₁ ∨ p₂ = (lit z₁∨lit z₂) ∧ (not(lit z₁)∨p₁) ∧ (not(lit z₂)∨p₂)
-   where (z₁:z₂:_) = newVariables (p₁ ++ p₂)
+  p₁ ∨ p₂ = satTo3sat =<< getCNF3 p₁ ∨ getCNF3 p₂
   
-  not (p₁:ps) = [[first (Any False<>) x] | x <- p₁] ∨ not ps
-  not [] = [[]]
+  not = satTo3sat <=< not . getCNF3
   
 
-satTo3sat :: CNFClause Integer -> CNF3 Integer
-satTo3sat p
-  | l<=3       = [CNF3Clause p]
-  | otherwise  = lit z ++ []
- where l = length p
-       (pl,pr) = splitAt (l`div`2) p
-       p'l = satTo3sat pl
-       p'r = satTo3sat pr
-       z = newVariable (p'l ++ p'r)
 
 
 
--- type NAE3CNF a = CNF3 a  -- interpreted differently: `:|` means
--- 
--- o3CNFToNAE3CNF :: CNF3Clause a -> CNF3 a
--- -- For a single disjunction, it's impossible
--- o3CNFToNAE3CNF (LitC n x) = [LitC n x :| LitC (not n) x]
--- o3CNFToNAE3CNF (LitC n x) = [LitC n x :| LitC (not n) x]
+type NAE3CNF a = CNF3 a  -- interpreted differently: `:|` means
+
+
+
+
+
 
 data SAT a = Lit a
            | Not (SAT a)
